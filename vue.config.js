620715c2ea7a1dd421b36a9f303868cfffe067e8
Antério Vieira <anteriovieira@gmail.com>
2019-09-23 12:21:32 -0300

    adds vue config file

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        App: path.resolve(__dirname, 'src/')
+      },
+      extensions: ['*', '.js', '.vue', '.json']
+    }
+  },
+  pwa: {
+    name: 'Traders Dash',
+    themeColor: '#131722',
+    msTileColor: '#FFFFFF',
+    background_color: '#131722',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    startUrl: '/?standalone=true'
+  }
+}
